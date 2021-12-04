0a3518196b7c815abb7e6ec234d7938922f34d10
Alexander Anker Christensen <alexander.Anker.chr@gmail.com>
2021-12-04 16:11:54 +0100

    Added titles

--- Stats.py
+++ Stats.py
@@ -2,11 +2,23 @@ import numpy as np
 
 ## Mean ##
 
-
-
 ## Standard Deviation ##
-
 
 ## Variance ##
 
+## Sample estimator of standard deviation ##
+
+## Standard Error of mean ##
+
+## Confidence interval for population mean (95%) ##
+
+## Standard Error of proportion ##
+
+## Confidence interval for proportion (95%) ##
+
+## Agresti-Coull interval for proportion (95%) ##
+
+## Sample size for mean (95%) ##
+
+## Sample size for proportion (95%) ##
 
